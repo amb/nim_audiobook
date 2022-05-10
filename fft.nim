6363793812f5c3ecd7e9b2b3383ec186eddf2122
Tommi Hyppänen <thyppane@gmail.com>
2022-05-10 18:14:22 +0300

    Power of 2 paddings

--- fft.nim
+++ fft.nim
@@ -41,14 +41,19 @@ proc fft0(n: int, s: int, eo: bool, x: ptr seq[Complex[float]], y: ptr seq[Compl
     let m: int = n div 2
     let theta0: float = 2.0*PI/float(n) 
 
-    if n == 2:
-        var z: ptr seq[Complex[float]] = if eo: y else: x
-        for q in 0..<s:
-            let a: Complex[float] = x[q + 0]
-            let b: Complex[float] = x[q + s]
-            z[q + 0] = a + b
-            z[q + s] = a - b
-    elif n >= 4:
+    # if n == 2:
+    #     var z: ptr seq[Complex[float]] = if eo: y else: x
+    #     for q in 0..<s:
+    #         let a: Complex[float] = x[q + 0]
+    #         let b: Complex[float] = x[q + s]
+    #         z[q + 0] = a + b
+    #         z[q + s] = a - b
+    # elif n >= 4:
+    if n == 1:
+        if eo:
+            for q in 0..<s:
+                y[q] = x[q]
+    else:
         for p in 0..<m:
             let fp = float(p)*theta0
             let wp = complex(cos(fp), -sin(fp))
@@ -62,13 +67,19 @@ proc fft0(n: int, s: int, eo: bool, x: ptr seq[Complex[float]], y: ptr seq[Compl
 proc fft*(xi: seq[Complex[float]]) =
     # n : sequence length
     # x : input/output sequence  
+
+    # Input length has to be a power of two
+    assert xi.len > 0
+    assert xi.len.isPowerOfTwo()
+    # assert ceil(log2(float(xi.len))) == floor(log2(float(xi.len)))
+    
     var n: int = xi.len
     var x = xi.unsafeAddr
     var y: seq[Complex[float]] = newSeq[Complex[float]](n)
     fft0(n, 1, false, x, y.addr)
-    let fn = complex(1.0/float(n))
-    for k in 0..<n:
-        x[k] *= fn
+    # let fn = complex(1.0/float(n))
+    # for k in 0..<n:
+    #     x[k] *= fn
 
 proc ifft*(xi: seq[Complex[float]]) =
     # n : sequence length
@@ -76,8 +87,10 @@ proc ifft*(xi: seq[Complex[float]]) =
     var n: int = xi.len
     var x = xi.unsafeAddr
 
+    let fn = complex(1.0/float(n))
     for p in 0..<n:
-        x[p] = x[p].conjugate
+        x[p] = (x[p]*fn).conjugate
+        # x[p] = x[p].conjugate
 
     var y: seq[Complex[float]] = newSeq[Complex[float]](n)
     fft0(n, 1, false, x, y.addr)
@@ -85,8 +98,16 @@ proc ifft*(xi: seq[Complex[float]]) =
     for p in 0..<n:
         x[p] = x[p].conjugate
 
+proc powerOf2Pad*(arr: seq[float]): seq[float] =
+    assert arr.len > 0
+    result = arr
+    for i in arr.len..<arr.len.nextPowerOfTwo:
+        result.add(arr[0])
+    assert result.len.isPowerOfTwo
+
 if isMainModule:
-    let tarr = @[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    let tarr = @[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 
+                -1.0,-1.0, 0.0, 0.0,-1.0,-1.0,-1.0,-1.0]
 
     for i in fft_slow(tarr):
         echo formatFloat(abs(i), ffDecimal, 3)
