413c75600b5dcafcb8e096bbe4ad376cfaa81d9b
Tommi Hyppänen <thyppane@gmail.com>
2022-05-17 20:42:51 +0300

    Bugfixes

--- fft.nim
+++ fft.nim
@@ -1,37 +1,45 @@
 import math, complex, strutils
-import std/[sequtils]
+import std/[sequtils, strformat, math]
 import vorbis, wavfile
 import benchy
 import arraymancer
 import x86_simd/x86_avx
 
-import pocketfft/pocketfft
+
+# {.experimental: "parallel".}
+# import std/threadpool
+
+# import pocketfft/pocketfft
 
 # import fftw3
+
+{.emit: """#include <valgrind/callgrind.h>""".}
 
 # OTFFT library
 # http://wwwa.pikara.ne.jp/okojisan/otfft-en/optimization1.html
 
 proc mulpz(ab: m256d, xy: m256d): m256d =
+    ## AVX float64 complex number multiplication
     let aa = unpacklo_pd(ab, ab)
     let bb = unpackhi_pd(ab, ab)
     let yx = shuffle_pd(xy, xy, 5)
     return addsub_pd(mul_pd(aa, xy), mul_pd(bb, yx))
 
-
 # This is +20-50% improvement
-const thetaLutSize = 2048
+const thetaLutSize = 32
 const thetaLut = static:
-    var arr: array[thetaLutSize, Complex[float]]
     let step = 2.0*PI/float(thetaLutSize)
+    var arr: array[thetaLutSize, array[2, float]]
     for k, v in mpairs(arr):
-        v = complex(cos(step * float(k)), -sin(step * float(k)))
+        v[0] = cos(step * float(k))
+        v[1] = -sin(step * float(k))
+    # var arr: array[thetaLutSize, Complex[float]]
+    # for k, v in mpairs(arr):
+    #     v = complex(cos(step * float(k)), -sin(step * float(k)))
     arr
-
 
 type
     FFTArray = seq[Complex[float]] | Tensor[Complex[float]]
-
 
 proc fft0[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
     ## Fast Fourier Transform
@@ -64,8 +72,8 @@ proc fft0[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
                 let b = x[q + s*(p+m)]
                 y[q + s*(2*p+0)] =  a + b
                 y[q + s*(2*p+1)] = (a - b) * wp
-        fft0(n div 2, 2 * s, not eo, y, x)
 
+        fft0(n div 2, 2 * s, not eo, y, x)
 
 proc fft0_avx[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
     ## Fast Fourier Transform
@@ -79,58 +87,71 @@ proc fft0_avx[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
     ##
     ## Returns:
     ## - Output sequence, either `x` or `y`
+    
+    {.emit: """CALLGRIND_START_INSTRUMENTATION; CALLGRIND_TOGGLE_COLLECT;""".}
 
-    var
-        ln = n
-        ls = s
-        leo = eo
+    let m = n div 2
 
-    while ln >= 4:
-        let theta0: float = 2.0*PI/float(ln)
-        ln = ln div 2
+    if n == 2:
+        if eo:
+            for q in 0..<s:
+                let a = x[q + 0]
+                let b = x[q + s]
+                y[q + 0] = a + b
+                y[q + s] = a - b
+        else:
+            for q in 0..<s:
+                let a = x[q + 0]
+                let b = x[q + s]
+                x[q + 0] = a + b
+                x[q + s] = a - b
 
-        if ls == 1:
-            for p in 0..<ln:
+    elif n >= 4:
+        let theta0: float = 2.0*PI/float(n)
+        let thetaL0: float = float(thetaLutSize)/float(n)
+        if s == 1:
+            for p in 0..<m:
                 let fp = float(p)*theta0
                 let wp = complex(cos(fp), -sin(fp))
                 let a = x[p+0]
-                let b = x[p+ln]
+                let b = x[p+m]
                 y[2*p+0] =  a + b
                 y[2*p+1] = (a - b) * wp
         else:
-            for p in 0..<ln:
-                let fp = float(p)*theta0
-                let cval =  cos(fp)
-                let sval = -sin(fp)
-                let o0 = ls*(p+0)
-                let o1 = ls*(p+ln)
-                let o2 = ls*(2*p+0)
-                let o3 = ls*(2*p+1)
+            for p in 0..<m:
+                let fpl  = thetaLut[int(float(p)*thetaL0)]
+                let cval = fpl[0]
+                let sval = fpl[1]
+
+                # thetaLutSize = 8
+                # let (cval, sval) = case int(float(p)*thetaL0)
+                # of 0: (1.0, -0.0)
+                # of 1: (0.7071067811865476, -0.7071067811865475)
+                # of 2: (0.0, -1.0)
+                # of 3: (-0.7071067811865475, -0.7071067811865476)
+                # of 4: (-1.0, 0.0)
+                # of 5: (-0.7071067811865477, 0.7071067811865475)
+                # of 6: (0.0, 1.0)
+                # of 7: (0.7071067811865474, 0.7071067811865477)
+                # else: (1.0, 1.0)
+
+                # let fp = float(p)*theta0
+                # let cval =  cos(fp)
+                # let sval = -sin(fp)
+                let o0 = s*(p+0)
+                let o1 = s*(p+m)
+                let o2 = s*(2*p+0)
+                let o3 = s*(2*p+1)
                 let wp = setr_pd(cval, sval, cval, sval)
-                for q in countup(0, ls-1, 2):
+                for q in countup(0, s-1, 2):
                     let a = load_pd_256(x[q+o0].re.addr)
                     let b = load_pd_256(x[q+o1].re.addr)
                     store_pd(y[q+o2].re.addr,           add_pd(a, b))
                     store_pd(y[q+o3].re.addr, mulpz(wp, sub_pd(a, b)))
 
-        ls = ls * 2
-        leo = not leo
-        (y, x) = (x, y)
+        fft0_avx(n div 2, s * 2, not eo, y, x)
 
-    if ln == 2:
-        var z = if eo: y else: x
-        if ls == 1:
-            let a = x[0]
-            let b = x[1]
-            z[0] = a + b
-            z[1] = a - b
-        else:
-            for q in 0..<ls:
-                let a = x[q +  0]
-                let b = x[q + ls]
-                z[q +  0] = a + b
-                z[q + ls] = a - b
-
+    {.emit: """CALLGRIND_TOGGLE_COLLECT; CALLGRIND_STOP_INSTRUMENTATION;""".}
 
 proc fft_empty_array(v: FFTArray): FFTArray =
     when v is seq:
@@ -138,14 +159,12 @@ proc fft_empty_array(v: FFTArray): FFTArray =
     elif v is Tensor:
         result = zeros[Complex[float]](v.shape[0])
 
-
 proc fft_array_len(v: FFTArray): int =
     result = 0
     when v is seq:
         result = v.len
     elif v is Tensor:
         result = v.shape[0]
-
 
 proc fft*(x: var FFTArray) =
     # n : sequence length
@@ -157,8 +176,7 @@ proc fft*(x: var FFTArray) =
     assert alen.isPowerOfTwo()
 
     var y = fft_empty_array(x)
-    fft0(alen, 1, false, x, y)
-
+    fft0_avx(alen, 1, false, x, y)
 
 proc ifft*(x: var FFTArray) =
     # n : sequence length
@@ -170,11 +188,10 @@ proc ifft*(x: var FFTArray) =
         x[p] = (x[p]*fn).conjugate
 
     var y = fft_empty_array(x)
-    fft0(n, 1, false, x, y)
+    fft0_avx(n, 1, false, x, y)
 
     for p in 0..<n:
         x[p] = x[p].conjugate
-
 
 proc padPowerOfTwo*(arr: seq[float]): seq[float] =
     assert arr.len > 0
@@ -182,7 +199,6 @@ proc padPowerOfTwo*(arr: seq[float]): seq[float] =
     for i in arr.len..<arr.len.nextPowerOfTwo:
         result.add(arr[0])
     assert result.len.isPowerOfTwo
-
 
 if isMainModule:
     echo "Running main module..."
@@ -192,10 +208,12 @@ if isMainModule:
 
     let fft_target = @[2.0, 6.15, 4.13, 3.4 , 1.41, 2.27, 1.71, 1.22,
                        0.0, 1.22, 1.71, 2.27, 1.41, 3.4 , 4.13, 6.15]
-
+   
     var ts = tarr.mapIt(complex(it, 0.0))
     fft(ts)
     var fft_result = ts.mapIt(round(abs(it), 2))
+    echo fft_target
+    echo fft_result
     doAssert fft_result == fft_target
     doAssert ts[0].re != tarr[0]
     ifft(ts)
@@ -203,6 +221,7 @@ if isMainModule:
     doAssert tsr == tarr
 
     var vsf = loadVorbis("data/sample.ogg").toFloat.padPowerOfTwo.mapIt(complex(it))
+    echo "Sample len: ", fft_array_len(vsf)
     var vsf2 = vsf
 
     var y = fft_empty_array(vsf)
@@ -210,6 +229,7 @@ if isMainModule:
         # Non-AVX is faster when -d:lto
         fft0(fft_array_len(vsf), 1, false, vsf, y)
         # fft0_avx(fft_array_len(vsf), 1, false, vsf, y)
+
 
     # Benchmark pocketFFT
     # var dOut = newSeq[Complex[float64]](vsf2.len)
@@ -222,3 +242,8 @@ if isMainModule:
     # nim c -d:release -d:lto -d:strip -d:danger -r fft.nim
     # nim c --cc:clang -d:release -d:danger --passC:"-flto" --passL:"-flto" -d:strip -r fft.nim && ll fft
     # https://github.com/kraptor/nim_callgrind
+    
+    # nim c -d:release fft.nim && valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes --collect-atstart=no --instr-atstart=no ./fft
+    
+    # import nimprof
+    # nim c -r -d:release --profiler:on --stackTrace:on fft.nim  
--- utils.nim
+++ utils.nim
@@ -1,4 +1,4 @@
-import arraymancer, pixie, sequtils, std/[strutils, strformat, base64], fft
+import arraymancer, pixie, sequtils, math, std/[strutils, strformat, base64], fft
 
 proc Image*(img: Image): string =
     return fmt"""<img src="data:image/png;base64, {img.encodeImage(PngFormat).encode}" />"""
@@ -25,7 +25,7 @@ proc plot1D*(img: Image, arr: openArray[float]): Image =
     img.strokePath(fmt"M {path[0]} L " & path.join(""), rgba(0, 0, 128, 255), strokeWidth = 1)
     return img
 
-proc basicSine*(srate: int): Tensor[system.float] =
+proc basicSine*(srate: int): Tensor[float] =
     let srate = float(srate)
     let dur = 1000
     let rr_dur = dur * 2
@@ -42,7 +42,30 @@ proc hann*(size: int): Tensor[float32] =
     let rr = toSeq(0..<size).mapIt(it.float32).toTensor()
     return cos(rr*2.0*PI/float(size))
 
-proc stft*(data: Tensor[float32], freq: float): Tensor[float32] =
+proc melBanks*[T: SomeFloat](rank, n, rate: int, fl, fh: T): Tensor[T] =
+    var temp = newTensor[T](1, rank + 2)
+    result = newTensor[T](rank, n)
+    for i in 0 ..< rank + 2:
+        temp[0, i] = T(n / rate) * mel2freq(freq2mel(fl) + T(i) * (freq2mel(fh) -
+            freq2mel(fl)) / T(rank+1))
+    for m in 1 .. rank:
+        for k in 0 .. n:
+            if temp[0, m - 1] <= T(k) and T(k) <= temp[0, m]:
+                result[m - 1, k] = (T(k) - temp[0, m - 1]) / (temp[0, m] - temp[0, m - 1])
+            elif temp[0, m] <= T(k) and T(k) <= temp[0, m + 1]:
+                result[m - 1, k] = (temp[0, m + 1] - T(k)) / (temp[0, m + 1] - temp[0, m])
+
+proc freq2mel*(freq: float): float = 
+    var value: float = 1.0 + freq / 700.0
+    if value == 0.0:
+        return 1.0/2048.0
+    else:
+        return 1127.0 * ln(value)
+
+proc mel2freq*(mel: float): float = 
+    return 700.0 * (exp(mel / 1127.0) - 1.0)
+
+proc stft*(data: Tensor[float], freq: float): Tensor[float] =
     # data: array containing the signal to be processed
     # freq: sampling frequency of the data
 
@@ -59,14 +82,14 @@ proc stft*(data: Tensor[float32], freq: float): Tensor[float32] =
     let window = hann(fft_size)
 
     # the zeros which will be used to double each segment size
-    let inner_pad = zeros[float32]([fft_size])
+    let inner_pad = zeros[float]([fft_size])
 
     # the data to process
-    let padding = zeros[float32]([pad_end_size])
+    let padding = zeros[float]([pad_end_size])
     let process = concat([data, padding], 0)
 
     # space to hold the result
-    result = zeros[float32]([total_segments, fft_size])
+    result = zeros[float]([total_segments, fft_size])
     echo result.shape
 
     # for each segment
@@ -83,7 +106,7 @@ proc stft*(data: Tensor[float32], freq: float): Tensor[float32] =
         # var spectrum: seq[Complex[float]] = padded.toSeq.mapIt(complex(float(it)))
         # fft(spectrum)
         # spectrum /= fft_size
-        let spectrum = zeros[Complex[float32]]([padded.shape[0]])
+        let spectrum = zeros[Complex[float]]([padded.shape[0]])
 
         let spec_conjugate = spectrum[_.._].map(conjugate)
         # find the autopower spectrum
@@ -92,9 +115,9 @@ proc stft*(data: Tensor[float32], freq: float): Tensor[float32] =
         result[i, _] = autopower[_..fft_size]
 
     # scale to db and clip
-    result = float32(20.0) * log10(result)
+    result = float(20.0) * log10(result)
     result = clamp(result, -40.0, 200.0)
 
 if isMainModule:
-    var cten = zeros[Complex[float32]]([5])
+    var cten = zeros[Complex[float]]([5])
     echo cten.shape, " ", cten
