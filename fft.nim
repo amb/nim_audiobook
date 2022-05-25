d1c46d89edc0ee4ccd79f46cb24a3b69eebb1fcf
Tommi Hyppänen <thyppane@gmail.com>
2022-05-25 22:21:41 +0300

    Remove custom asm code

--- fft.nim
+++ fft.nim
@@ -1,40 +1,22 @@
 import math, complex
 # import arraymancer
-#, x86_simd/x86_avx
 
 when isMainModule:
     import benchy, strformat, strutils, sequtils
     import audiofile/[vorbis, wavfile]
 
-# {.experimental: "parallel".}
-# import std/threadpool
-
 # import pocketfft/pocketfft
-
 # import fftw3
-
-# {.emit: """#include <valgrind/callgrind.h>""".}
 
 # OTFFT library
 # http://wwwa.pikara.ne.jp/okojisan/otfft-en/optimization1.html
 
-# proc mulpz(ab: m256d, xy: m256d): m256d {.inline.} =
-#     ## AVX float64 complex number multiplication
-#     let aa = unpacklo_pd(ab, ab)
-#     let bb = unpackhi_pd(ab, ab)
-#     let yx = shuffle_pd(xy, xy, 5)
-#     return addsub_pd(mul_pd(aa, xy), mul_pd(bb, yx))
-
 const thetaLutSize = 2048
 const thetaLut = static:
     let step = 2.0*PI/float(thetaLutSize)
-    var arr: array[thetaLutSize, array[2, float]]
+    var arr: array[thetaLutSize, Complex[float]]
     for k, v in mpairs(arr):
-        v[0] = cos(step * float(k))
-        v[1] = -sin(step * float(k))
-    # var arr: array[thetaLutSize, Complex[float]]
-    # for k, v in mpairs(arr):
-    #     v = complex(cos(step * float(k)), -sin(step * float(k)))
+        v = complex(cos(step * float(k)), -sin(step * float(k)))
     arr
 
 # thetaLutSize = 8
@@ -66,18 +48,18 @@ proc fft0*[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
     ## - Output sequence, either `x` or `y`
 
     let m: int = n div 2
-    # let theta0: float = 2.0*PI/float(n)
-    let thetaL0: float = float(thetaLutSize)/float(n)
+    let theta0: float = 2.0*PI/float(n)
+    # let thetaL0: float = float(thetaLutSize)/float(n)
     if n == 1:
         if eo:
             for q in 0..<s:
                 y[q] = x[q]
     else:
         for p in 0..<m:
-            # let fp = float(p)*theta0
-            # let wp = complex(cos(fp), -sin(fp))
-            let fpl = thetaLut[int(float(p)*thetaL0)]
-            let wp  = complex(fpl[0], fpl[1])
+            let fp = float(p)*theta0
+            let wp = complex(cos(fp), -sin(fp))
+            # let wp = thetaLut[int(float(p)*thetaL0)]
+            # let wp  = complex(fpl[0], fpl[1])
             for q in 0..<s:
                 let a = x[q + s*(p+0)]
                 let b = x[q + s*(p+m)]
@@ -85,72 +67,6 @@ proc fft0*[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
                 y[q + s*(2*p+1)] = (a - b) * wp
 
         fft0(n div 2, 2 * s, not eo, y, x)
-
-# proc fft0_avx*[T: FFTArray](n: int, s: int, eo: bool, x: var T, y: var T) =
-#     ## Fast Fourier Transform
-#     ##
-#     ## Inputs:
-#     ## - `n` Sequence length. **Must be power of two**
-#     ## - `s` Stride
-#     ## - `eo` x is output if eo == 0, y is output if eo == 1
-#     ## - `x` Input sequence(or output sequence if eo == 0)
-#     ## - `y` Work area(or output sequence if eo == 1)
-#     ##
-#     ## Returns:
-#     ## - Output sequence, either `x` or `y`
-    
-#     # {.emit: """CALLGRIND_START_INSTRUMENTATION; CALLGRIND_TOGGLE_COLLECT;""".}
-
-#     let m = n div 2
-
-#     if n == 2:
-#         if eo:
-#             for q in 0..<s:
-#                 let a = x[q + 0]
-#                 let b = x[q + s]
-#                 y[q + 0] = a + b
-#                 y[q + s] = a - b
-#         else:
-#             for q in 0..<s:
-#                 let a = x[q + 0]
-#                 let b = x[q + s]
-#                 x[q + 0] = a + b
-#                 x[q + s] = a - b
-
-#     elif n >= 4:
-#         let theta0: float = 2.0*PI/float(n)
-#         let thetaL0: float = float(thetaLutSize)/float(n)
-#         if s == 1:
-#             for p in 0..<m:
-#                 let fp = float(p)*theta0
-#                 let wp = complex(cos(fp), -sin(fp))
-#                 let a = x[p+0]
-#                 let b = x[p+m]
-#                 y[2*p+0] =  a + b
-#                 y[2*p+1] = (a - b) * wp
-#         else:
-#             for p in 0..<m:
-#                 let fpl  = thetaLut[int(float(p)*thetaL0)]
-
-#                 # let fp = float(p)*theta0
-#                 # let cval =  cos(fp)
-#                 # let sval = -sin(fp)
-
-#                 let o0 = s*(p+0)
-#                 let o1 = s*(p+m)
-#                 let o2 = s*(2*p+0)
-#                 let o3 = s*(2*p+1)
-#                 # let wp = setr_pd(cval, sval, cval, sval)
-#                 let wp = setr_pd(fpl[0], fpl[1], fpl[0], fpl[1])
-#                 for q in countup(0, s-1, 2):
-#                     let a = load_pd_256(x[q+o0].re.addr)
-#                     let b = load_pd_256(x[q+o1].re.addr)
-#                     store_pd(y[q+o2].re.addr,           add_pd(a, b))
-#                     store_pd(y[q+o3].re.addr, mulpz(wp, sub_pd(a, b)))
-
-#         fft0_avx(n div 2, s * 2, not eo, y, x)
-
-#     # {.emit: """CALLGRIND_TOGGLE_COLLECT; CALLGRIND_STOP_INSTRUMENTATION;""".}
 
 proc fft_empty_array*(v: FFTArray): FFTArray =
     when v is seq:
@@ -225,16 +141,10 @@ when isMainModule:
     var vsf = loadVorbis("data/sample.ogg").toFloat.padPowerOfTwo
     var vsf2 = vsf.mapIt(complex(it))
     echo "Sample len: ", fft_array_len(vsf2)
-    # var vsf2 = vsf
 
     var y = fft_empty_array(vsf2)
     timeIt "fft":
-        # Non-AVX is faster when -d:lto
         fft0(fft_array_len(vsf2), 1, false, vsf2, y)
-
-    # timeIt "fft_avx":
-    #     # Faster when only -d:release or debug
-    #     fft0_avx(fft_array_len(vsf), 1, false, vsf, y)
 
     # Benchmark pocketFFT
     # var dOut = newSeq[Complex[float64]](vsf2.len)
