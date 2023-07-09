d137037147a6207648f663ae35d40be0eb25831d
Tommi Hyppänen <thyppane@gmail.com>
2023-07-09 22:55:20 +0300

    Using the existing C-APi

---
+++ otfft-simple/nim_test.nim
@@ -0,0 +1,37 @@
+import std/[math, complex, times, strformat]
+import otfft_c
+
+const
+    n_max = 22
+    N_max = 1 shl n_max
+    nN_max = n_max * N_max
+
+proc imin(x, y: int): int =
+    if x < y: x else: y
+
+proc main() =
+    var x = newSeq[Complex[float64]](N_max)
+    for n in 1..n_max:
+        stdout.write fmt"2^({n}): "        
+        let N = 1 shl n
+        let LOOPS = imin(70, n * 4) * (nN_max div (n * N))
+        for p in 0..<N:
+            let t = p.float / N.float
+            x[p] = complex(
+                10.0 * cos(3.0 * 2.0 * PI * t * t), 
+                10.0 * sin(3.0 * 2.0 * PI * t * t))
+
+        var obj = otfft_fft_new(N.cint)
+
+        let t1 = epochTime()
+        let xaddr = x[0].addr
+        for i in 0..<LOOPS:
+            obj.otfft_fft_fwd(xaddr)
+            obj.otfft_fft_inv(xaddr)
+        let t2 = epochTime()
+        
+        let tusec = float(t2 - t1) * 1000000.0 / float(LOOPS)
+        echo fmt"{tusec:.3f} [usec], {LOOPS} loops"
+
+when isMainModule:
+    main()
---
+++ otfft-simple/otfft_c.nim
@@ -0,0 +1,81 @@
+## ****************************************************************************
+##   OTFFT C Header Version 11.5e
+##
+##   Copyright (c) 2016 OK Ojisan(Takuya OKAHISA)
+##   Released under the MIT license
+##   http://opensource.org/licenses/mit-license.php
+## ****************************************************************************
+
+# gcc -O3 -c hello.c && gcc hello.o otfft/otfft.o -lgomp -lm -lstdc++ -o hello && ./hello
+
+{.passL:"otfft.o".}
+{.passL:"-lgomp".}
+{.passL:"-lm".}
+{.passL:"-lstdc++".}
+
+import std/complex
+
+proc simd_malloc*(n: csize_t): pointer {.importc, cdecl.}
+proc simd_free*(p: pointer) {.importc, cdecl.}
+
+proc otfft_fft_new*(N: cint): pointer {.importc, cdecl.}
+proc otfft_fft_delete*(p: pointer) {.importc, cdecl.}
+proc otfft_fft_fwd*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_fwd0*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_fwdu*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_fwdn*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_inv*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_inv0*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_invu*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+proc otfft_fft_invn*(p: pointer; x: ptr Complex64) {.importc, cdecl.}
+
+# proc otfft_fft0_new*(N: cint): pointer
+# proc otfft_fft0_delete*(p: pointer)
+# proc otfft_fft0_fwd*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_fwd0*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_fwdu*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_fwdn*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_inv*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_inv0*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_invu*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+# proc otfft_fft0_invn*(p: pointer; x: ptr Complex64; y: ptr Complex64)
+
+# proc otfft_rfft_new*(N: cint): pointer
+# proc otfft_rfft_delete*(p: pointer)
+# proc otfft_rfft_fwd*(p: pointer; x: ptr cdouble; y: ptr Complex64)
+# proc otfft_rfft_fwd0*(p: pointer; x: ptr cdouble; y: ptr Complex64)
+# proc otfft_rfft_fwdu*(p: pointer; x: ptr cdouble; y: ptr Complex64)
+# proc otfft_rfft_fwdn*(p: pointer; x: ptr cdouble; y: ptr Complex64)
+# proc otfft_rfft_inv*(p: pointer; x: ptr Complex64; y: ptr cdouble)
+# proc otfft_rfft_inv0*(p: pointer; x: ptr Complex64; y: ptr cdouble)
+# proc otfft_rfft_invu*(p: pointer; x: ptr Complex64; y: ptr cdouble)
+# proc otfft_rfft_invn*(p: pointer; x: ptr Complex64; y: ptr cdouble)
+
+# proc otfft_dct_new*(N: cint): pointer
+# proc otfft_dct_delete*(p: pointer)
+# proc otfft_dct_fwd*(p: pointer; x: ptr cdouble)
+# proc otfft_dct_fwd0*(p: pointer; x: ptr cdouble)
+# proc otfft_dct_fwdn*(p: pointer; x: ptr cdouble)
+# proc otfft_dct_inv*(p: pointer; x: ptr cdouble)
+# proc otfft_dct_inv0*(p: pointer; x: ptr cdouble)
+# proc otfft_dct_invn*(p: pointer; x: ptr cdouble)
+
+# proc otfft_dct0_new*(N: cint): pointer
+# proc otfft_dct0_delete*(p: pointer)
+# proc otfft_dct0_fwd*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+# proc otfft_dct0_fwd0*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+# proc otfft_dct0_fwdn*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+# proc otfft_dct0_inv*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+# proc otfft_dct0_inv0*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+# proc otfft_dct0_invn*(p: pointer; x: ptr cdouble; y: ptr cdouble; z: ptr Complex64)
+
+# proc otfft_bluestein_new*(N: cint): pointer
+# proc otfft_bluestein_delete*(p: pointer)
+# proc otfft_bluestein_fwd*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_fwd0*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_fwdu*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_fwdn*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_inv*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_inv0*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_invu*(p: pointer; x: ptr Complex64)
+# proc otfft_bluestein_invn*(p: pointer; x: ptr Complex64)
